3e3ce883fc9892fd8e39cd617a40400acde39f82
Da Yeon Hwang <dahwang2018@u.northwestern.edu>
2018-11-30 11:13:06 -0600

    add plotting

--- ImageMosaic.m
+++ ImageMosaic.m
@@ -1,19 +1,21 @@
 %% Use SIFT to get matches between images
-img1 = imread('test2_1.jpg');
-img2 = imread('test2_2.jpg');
+img1 = imread('test1_1.png');
+img2 = imread('test1_2.png');
 
-[f1, d1] = vl_sift(single(rgb2gray(img1))); % [x, y, s, th]
+[f1, d1] = vl_sift(single(rgb2gray(img1))); % f: [x, y, s, th]
 [f2, d2] = vl_sift(single(rgb2gray(img2)));
 matches = vl_ubcmatch(d1,d2); % [index in f1, index in f2]
-
+numMatches = size(matches,2); % number of matches
 
 %% Use RANSAC for homography computation
 % Iterate N times
 N = 100; % number of times to iterate
 t = 3; % threshold for whether pixel matches
+allCount = {}; % array to hold all inliers
+allH = {}; % array to hold all Hs
 count = 0; % number of matches for currH
+maxInliers = 0; % maximum inliers
 currH = zeros(3,3); % H with most inliers - current best
-
 for n=1:N
     % Randomly select a sample
     index=randi(size(matches,2));
@@ -48,8 +50,45 @@ for n=1:N
         end
     end
     
+    % Save current homography's inliers
+    allCount{n} = numInliers;
+    allH{n} = H;
+    
+    
+    % Update maximum number of inliers
     if numInliers>count
         count = numInliers;
         currH = H;
     end
 end
+
+% % Get the best H and number of inliers
+% [allCount,ind] = max(allCount);
+% H = H{ind};
+% maxInliers = allCount{ind};
+% sprintf('%d max inliers', maxInliers);
+
+%% Visualize all matches
+% Pad if widths of two images are different
+width_diff1 = max(size(img2,1)-size(img1,1),0);
+width_diff2 = max(size(img1,1)-size(img2,1),0);
+
+% Plot match lines
+figure();
+imagesc([padarray(img1,width_diff1,'post') padarray(img2,width_diff2,'post')]);
+img1_height = size(img1,2);
+line([f1(1,matches(1,:));f2(1,matches(2,:))+img1_height],[f1(2,matches(1,:));f2(2,matches(2,:))]);
+title(sprintf('%d matches', numMatches));
+axis image off;
+
+drawnow;
+
+%% Visualize inlier matches
+figure();
+imagesc([padarray(img1,width_diff1,'post') padarray(img2,width_diff2,'post')]);
+img1_height = size(img1,2);
+line([f1(1,matches(1,count));f2(1,matches(2,count))+img1_height],[f1(2,matches(1,count));f2(2,matches(2,count))]);
+title(sprintf('%d inliner matches', count));
+axis image off;
+
+drawnow ;
